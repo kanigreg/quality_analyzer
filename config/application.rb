dd4e34863fac94785081ae756e7604c063ad68a8
kanigreg <kanigreg@gmail.com>
2022-12-01 18:52:02 +0500

    Specify default host

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,7 @@ module QualityAnalyzer
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
-    routes.default_url_options = { host: ENV.fetch('BASE_URL', nil) }
+    routes.default_url_options = { host: ENV.fetch('BASE_URL', 'localhost') }
 
     # Configuration for the application, engines, and railties goes here.
     #
